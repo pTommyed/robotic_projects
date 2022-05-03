acb02d9f68890f062458e1d897fd62f9c2901f46
pTommyed <35973997+pTommyed@users.noreply.github.com>
2022-05-03 21:36:50 +0200

    can init compelete, can readmessage complete, can clearing messing complete, cycle counter implemetnted,

--- spider/encoder/encoder.ino
+++ spider/encoder/encoder.ino
@@ -7,17 +7,22 @@
 
 // DESCRTIPTION: -   sampling frequency 5 kHz
 //               -   timer 0 -- sampling freq
-//               -   timer 2 -- printing values freq
+//               -   CAN bus implamantaton (library)
 
-// HW: Aruino nano
+// HW: Aruino nano, mcp2515
 
 /*----------------------- DEPENDENCES ----------------------------------*/
+#include <mcp_can.h> // CAN lib
+#include <SPI.h> // lib for SPI bus
+
 
 /*----------------------- DEFINITION -----------------------------------*/
 byte counter1_channelA = A0;
 byte counter1_channelB = A1;
 byte counter2_channelA = A2;
 byte counter2_channelB = A3;
+
+byte cs_pin = 10; // SPI CS pin number
 
 int enc_counter_1 = 0; // value of first encoder
 int enc_counter_2 = 0; // value of secondencoder
@@ -29,25 +34,38 @@ static int encoder_status_tabel [] = {0,-1,1,10,1,0,10,-1,-1,10,0,1,10,1,-1,0};
 
 
 bool timer0_flag = 0;
+bool nmt_recive_flag = 0;
 
 
+MCP_CAN CAN(cs_pin);
+
+static int can_adress_pdo = 131; //transmit encoder counters values
+static int can_adress_hb = 130; //  transmit HB adress
+static int can_adress_nmt = 0; // Sync adress
+int  can_message_id_recive; //can message of receive message
+byte buf_recive[8]; //buffer of receive message
+byte buf_transmit_hb[2] = {0,0}; //buffer of transmit message
+byte buf_transmit_pdo[8]; //buffer of transmit message
+unsigned char len = 0; // lenght of receive message
+
+byte cycle_counter = 0; 
+
+byte read_message_error = 1;
+byte clear_message_error = 2;
 
 /*----------------------- SETUP -----------------------------------*/
 void setup () {
   serial_initial();
   pins_initial();
   encoders_init();
+  can_initial();
   timer0_initial();
 }
 
 /*----------------------- LOOP -----------------------------------*/
 void loop (){
-  if (timer0_flag == 1) {
-    timer0_flag = 0;
-    
-    encoder_read();
-    encoders_counting();
-    //serial_prnt();
-  }
-
+  counter_cycle();
+  timer0_flag_check ();
+  can_message_recive_check ();
+  buf_transmit_hb_clear();
 }
--- spider/encoder/methods.ino
+++ spider/encoder/methods.ino
@@ -6,11 +6,82 @@ void serial_initial() {
   }
 }
 
-/*----------------------serial-print------------------------------------*/
-void serial_prnt() {
-  Serial.print(enc_counter_1);
-  Serial.print(" ## ");
-  Serial.println(enc_counter_2);
+/*-----------------------CAN initialization--------------------------------*/
+void can_initial(){
+  Serial.println("Setup can..");
+  while(CAN.begin(CAN_500KBPS, MCP_8MHz) != CAN_OK){
+    Serial.print(".");
+    delay(1000);
+  }
+
+  can_mask_initial();
+  can_filters_initial();
+  Serial.print("\nCAN init ok!!\r\n");
+
+}
+
+/*-----------------------CAN Mask initialization--------------------------------*/
+void can_mask_initial(){
+  while(CAN.init_Mask(0, 0, 0xffff) != 0){
+    Serial.print("-");
+    delay(109);
+  }
+  while(CAN.init_Mask(1, 0, 0xffff) != 0) {
+    Serial.print("-");
+    delay(100);
+  } 
+}
+
+/*-----------------------CAN filters initialization--------------------------------*/
+void can_filters_initial(){
+  while(CAN.init_Filt(0, 0, 0x00) != 0){
+    Serial.print("-");
+    delay(10);
+  }
+  while(CAN.init_Filt(1, 0, 0x00) != 0){
+    Serial.print("-");
+    delay(100);
+  }
+  
+  
+  
+  while(CAN.init_Filt(2, 0, 0x00) != 0){
+    Serial.print("-");
+    delay(100);
+  }
+  while(CAN.init_Filt(3, 0, 0x00) != 0){
+    Serial.print("-");
+    delay(100);
+  }
+  while(CAN.init_Filt(4, 0, 0x00) != 0){
+    Serial.print("-");
+    delay(100);
+  }
+  while(CAN.init_Filt(5, 0, 0x00) != 0){
+    Serial.print("-");
+    delay(100);
+  }
+}
+
+/*--------------------------timer0-initialization------------------------------------------*/
+void timer0_initial() {
+  cli();  // disable all interrupts
+
+  OCR0A = 50;                                         // compareregister 16MHz/64/5kHz = 50 (max 255)
+  TCCR0B |= (1 << CS00) | (1 << CS01) | (0 << CS02);  // 64 prescaler
+  TIMSK0 |= (1 << OCIE0A);                            // enable timer interrupt ( interrupt is active- when is value of TCNT0 (value of counter) in match with OCR0A)
+
+  sei();  // enable all interrupts
+}
+
+/*--------------------------interupt_function_timer_0----------------*/
+ISR(TIMER0_COMPA_vect) {
+  timer0_flag = 1;
+}
+
+/*----------------------encoders_initialization------------------------------------*/
+void encoders_init() {
+  encoder_read();
 }
 
 /*----------------------pins-initialization------------------------------------*/
@@ -19,6 +90,104 @@ void pins_initial() {
   pinMode(counter1_channelB, INPUT);
   pinMode(counter2_channelA, INPUT);
   pinMode(counter2_channelB, INPUT);
+}
+
+
+/*-------------------------- timer0_flag_status_check  -------------------------*/
+
+void timer0_flag_check () {
+  if (timer0_flag == 1) {
+    timer0_flag = 0;
+    
+    encoder_read();
+    encoders_counting();
+    //serial_prnt_encoders();
+  }
+}
+
+/*-------------------------- can_message_recive_check -------------------------*/
+
+void can_message_recive_check () {
+  if (CAN.checkReceive() == CAN_MSGAVAIL) {
+    can_message_recive();
+    can_message_clear();
+    can_msg_to_serial();
+    serial_prnt_buf_transmit_hb();
+  }
+}
+
+/*-------------------------- can_message_clear -------------------------*/
+
+void can_message_clear() {
+  while (CAN.clearMsg() != MCP2515_OK) {
+    if (timer0_flag == 1) {
+      buf_transmit_hb[1] = clear_message_error;
+      return;
+    }
+  }
+}
+
+/*-------------------------- can message recive -------------------------*/
+
+void can_message_recive() {
+  can_message_id_recive = CAN.getCanId();
+    if (can_message_id_recive == can_adress_nmt) {
+     nmt_recive_flag = 1;
+     while (CAN.readMsgBuf(&len, buf_recive) != CAN_OK) {
+       if (timer0_flag == 1) {
+         buf_transmit_hb[1] = read_message_error;
+         return;
+       }
+     }
+    }
+}
+
+/*----------------------serial-print_recive_can_message------------------------------------*/
+
+void can_msg_to_serial(){
+      Serial.println("-----------------------------------------");
+      Serial.print("can address: ");
+      Serial.print(can_message_id_recive);
+      Serial.print("; lenght: ");
+      Serial.println(len);
+      for (int i=0;i<len;i++){
+        Serial.print(buf_recive[i]);
+        Serial.print("\t");
+      }
+      Serial.println("");
+      Serial.println("-----------------------------------------");
+}
+
+/*----------------------serial-print_encoders------------------------------------*/
+void serial_prnt_encoders() {
+  Serial.print(enc_counter_1);
+  Serial.print(" ## ");
+  Serial.println(enc_counter_2);
+}
+
+/*----------------------serial-print_buf_transmit_hb------------------------------------*/
+void serial_prnt_buf_transmit_hb() {
+  Serial.print(buf_transmit_hb[0]);
+  Serial.print("##");
+  Serial.println(buf_transmit_hb[1]);
+}
+
+/*---------------------------- counter_cycle--------------------------------------------------------------------------------------------------*/
+
+void counter_cycle(){
+  buf_transmit_hb [0] = cycle_counter;
+  if (cycle_counter == 255){
+    cycle_counter = 1;
+  } else {
+     cycle_counter = cycle_counter + 1;
+    }
+}
+
+/*--------------------------- buf_transmit_hb_clear----------------------------------------------------------------------------------*/
+
+void buf_transmit_hb_clear(){
+  buf_transmit_hb [0] = 0;
+  buf_transmit_hb [1] = 0;
 }
 
 /*----------------------encoder_read------------------------------------*/
@@ -52,29 +221,8 @@ void encoder_read() {
   encoder2_status = encoder2_status | temp_B;
 }
 
-/*----------------------encoders_initialization------------------------------------*/
-void encoders_init() {
-  encoder_read();
-}
-
 /*----------------------encoders_counting------------------------------------*/
 void encoders_counting() {
   enc_counter_1 = enc_counter_1 + encoder_status_tabel[encoder1_status];
   enc_counter_2 = enc_counter_2 + encoder_status_tabel[encoder2_status];
-}
-
-/*--------------------------interupt_function_timer_0----------------*/
-ISR(TIMER0_COMPA_vect) {
-  timer0_flag = 1;
-}
-
-/*--------------------------timer0-initialization------------------------------------------*/
-void timer0_initial() {
-  cli();  // disable all interrupts
-
-  OCR0A = 50;                                         // compareregister 16MHz/64/5kHz = 50 (max 255)
-  TCCR0B |= (1 << CS00) | (1 << CS01) | (0 << CS02);  // 64 prescaler
-  TIMSK0 |= (1 << OCIE0A);                            // enable timer interrupt ( interrupt is active- when is value of TCNT0 (value of counter) in match with OCR0A)
-
-  sei();  // enable all interrupts
 }
